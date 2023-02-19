eb2385ed996f421053145e361152f494e7393ddf
sylverb <sylver.bruneau@gmail.com>
2023-02-19 20:07:40 +0100

    Add new nes texts in de and ru files (not translated yet)

--- Core/Src/retro-go/i18n/rg_i18n_ru_ru.c
+++ Core/Src/retro-go/i18n/rg_i18n_ru_ru.c
@@ -31,6 +31,19 @@ const lang_t lang_ru_ru LANG_DATA = {
     .extra_font = cp1251_fonts,
     .s_LangUI = "язык",
     .s_LangName = "Russian",
+
+     // Core\Src\porting\nes-fceu\main_nes_fceu.c ===========================
+    .s_Crop_Vertical_Overscan = "Crop Vertical Overscan",
+    .s_Crop_Horizontal_Overscan = "Crop Horizontal Overscan",
+    .s_Disable_Sprite_Limit = "Disable sprite limit",
+    .s_NES_CPU_OC = "NES CPU Overclocking",
+    .s_NES_Eject_Insert_FDS = "Eject/Insert Disk",
+    .s_NES_Eject_FDS = "Eject Disk",
+    .s_NES_Insert_FDS = "Insert Disk",
+    .s_NES_Swap_Side_FDS = "Swap FDisk side",
+    .s_NES_FDS_Side_Format = "Disk %d Side %s",
+    //=====================================================================
+
     // Core\Src\porting\gb\main_gb.c =======================================
     .s_Palette = "ѕалитра",
     //=====================================================================
