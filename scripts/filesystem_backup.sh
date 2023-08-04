8c2b8020ba01c5d078e55a0319f8ce554ca6a8f8
Brian Pugh <bnp117@gmail.com>
2023-08-04 11:36:52 -0700

    remove old scripts

--- scripts/filesystem_backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# TODO: Currently the location and size is hardcoded; need to make dynamic
-# Just performs a complete dump of the filesystem partition; needs to be read
-# using something like littlefs-python
-. ./scripts/common.sh
-
-${OPENOCD} -f scripts/interface_${ADAPTER}.cfg \
-    -c "init; halt; dump_image fs.bin 0x90300000 1048576; resume; exit;"
