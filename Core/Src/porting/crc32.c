6f3a2bb47517307d53c3612cf737355f29a88742
Sylver Bruneau <sylverbruneau@MacBook-Pro-de-Sylver.local>
2024-08-14 14:33:13 +0200

    More intflash optimizations and corrections

--- Core/Src/porting/crc32.c
+++ Core/Src/porting/crc32.c
@@ -11,11 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#include "main.h"
 #include <stdint.h>
 #include <stdbool.h>
 #include "porting.h"
 
-DEXTFLASH_ATTR static const unsigned int crc32_le_table[256] = {
+DEXTFLASH_ATTR static const unsigned int crc32_le_table[256] GLOBAL_DATA = {
     0x00000000L, 0x77073096L, 0xee0e612cL, 0x990951baL, 0x076dc419L, 0x706af48fL, 0xe963a535L, 0x9e6495a3L,
     0x0edb8832L, 0x79dcb8a4L, 0xe0d5e91eL, 0x97d2d988L, 0x09b64c2bL, 0x7eb17cbdL, 0xe7b82d07L, 0x90bf1d91L,
     0x1db71064L, 0x6ab020f2L, 0xf3b97148L, 0x84be41deL, 0x1adad47dL, 0x6ddde4ebL, 0xf4d4b551L, 0x83d385c7L,
